43182e56d1be4262d0acbd01d2bc4672b9bea076
Muchael123 <mainamichael2849@gmail.com>
2024-02-27 15:50:05 +0300

    My App

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full mt-5  md:px-12 lg:px-20 bg-blue-500 text-white'>
+    <div className='w-full mt-0  md:px-12 lg:px-20 bg-blue-500 text-white'>
         <hr className='w-full bg-gray-800 h-[2px]'></hr>
         <div className='w-full items-center flex-row text-xs md:text-[12px] py-4 justify-center flex'>
             <p>Made with Love<span>❤🚀</span></p>
