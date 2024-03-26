52bca361b799f03b24ea89efc61b0a54e4f501ee
Muchael123 <mainamichael2849@gmail.com>
2024-03-26 15:37:44 +0300

    Last commit

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -11,7 +11,6 @@ function Login() {
     event.preventDefault();
     const notification = toast.loading("authenticating...");
     try {
-      
       const response = await fetch(
         "https://tumeiget.vercel.app/account/login/",
         {
@@ -113,9 +112,6 @@ function Login() {
                 >
                   Password
                 </label>
-                <a class="ml-auto inline-block text-sm underline" href="#">
-                  Forgot your password?
-                </a>
               </div>
               <input
                 class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
