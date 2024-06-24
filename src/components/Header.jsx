e701f3b67c973cd6b4f0d2e0ea36edb3aa0061b2
Maich_magodeh <Mainamichael2849@gmail.com>
2024-06-24 06:56:59 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
   
         {/* Right */}
         <div className='flex text-xs md:text-base divide-x'>
-            <a href="https://my-portfolio-sepia-six.vercel.app/" className='px-2 font-light tetx-right hover:text-blue-500 transition-all duration-300 ease-in-out'>See Our Portfolio</a>
+            <a href="https://michael-maina.me" className='px-2 font-light tetx-right hover:text-blue-500 transition-all duration-300 ease-in-out'>See Our Portfolio</a>
             <a href="https://github.com/Muchael123" className='font-light px-2 hover:text-blue-500 transition-all duration-300 ease-in-out'>Our Github Repo</a>
         </div>
     </div>
